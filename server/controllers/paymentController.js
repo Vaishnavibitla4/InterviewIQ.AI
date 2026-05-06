ac53323acb24324baff8fc1c28311ee5ee75d44a
Vaishnavibitla4 <bitlavaishnavi4@gmail.com>
2026-05-06 13:23:54 +0530

    fix: correct import path for Payment model in paymentController

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,5 +1,5 @@
 import razorpay from '../services/razorpay.service.js';
-import Payment from '../models/Payment.js';
+import Payment from '../models/payment.js';
 import crypto from 'crypto';
 import User from '../models/user.js';
 
